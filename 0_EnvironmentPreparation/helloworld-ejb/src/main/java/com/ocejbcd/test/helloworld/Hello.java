c30e6e55ea4c0ab4ae39c3a3185bd8025da07ef9
asad <asad@asad-HP-Pavilion-dv6-Notebook-PC>
2016-07-08 18:43:46 +0500

    small changes for plugins

--- 0_EnvironmentPreparation/helloworld-ejb/src/main/java/com/ocejbcd/test/helloworld/Hello.java
+++ 0_EnvironmentPreparation/helloworld-ejb/src/main/java/com/ocejbcd/test/helloworld/Hello.java
@@ -3,6 +3,6 @@ package com.ocejbcd.test.helloworld;
 import javax.ejb.Remote;
 
 @Remote
-public interface Hello{
-    public String sayHello();
+public interface Hello {
+	public String sayHello();
 }
--- 0_EnvironmentPreparation/helloworld-ejb/src/main/java/com/ocejbcd/test/helloworld/HelloBean.java
+++ 0_EnvironmentPreparation/helloworld-ejb/src/main/java/com/ocejbcd/test/helloworld/HelloBean.java
@@ -2,9 +2,15 @@ package com.ocejbcd.test.helloworld;
 
 import javax.ejb.Stateless;
 
+/**
+ * Session Bean implementation class HelloBean
+ */
 @Stateless
-public class HelloBean implements Hello{
-    public String sayHello(){
-        return "Hello World!!!!";
-    }
+public class HelloBean implements Hello {
+
+	@Override
+	public String sayHello() {
+		return "Hello World!!!!";
+	}
+
 }
